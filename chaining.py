4dacb227a8deb8d5942c01045fc7b3b1147cda5e
sofiansmendes <ana.sofia.ns.mendes@gmail.com>
2024-03-18 16:19:19 +0100

    clf in the middle of the chain is now working :)

--- chaining.py
+++ chaining.py
@@ -56,6 +56,9 @@ class Chain(BaseEstimator):
             # Propagate the predictions
             if self.propagate == "pred":
                 yi_pred = model.predict(Xext)
+                if yi_pred.dtype == 'object':
+                    yi_pred = pd.Categorical(yi_pred).codes
+                    yi_pred = pd.Series(yi_pred).replace(-1, np.nan, inplace=True)
                 Xext[col] = yi_pred
             elif self.propagate == "true":
                 Xext[col] = y[col]
@@ -70,10 +73,17 @@ class Chain(BaseEstimator):
         if not isinstance(X, pd.DataFrame):
             X = pd.DataFrame(X, columns=[f"X{i}" for i in range(X.shape[1])])
         Xext = X.copy()
+        pred_list = X.copy()
         for col, model in zip(self.y_columns, self.models):
             if self.propagate == False:
                 yi_pred = model.predict(X)
             else:
                 yi_pred = model.predict(Xext)
-            Xext[col] = yi_pred
-        return Xext.iloc[:, -len(self.models):]
+                if yi_pred.dtype == 'object':
+                    yi_pred_num = pd.Categorical(yi_pred).codes
+                    yi_pred_num = pd.Series(yi_pred_num).replace(-1, np.nan, inplace=True)
+                    Xext[col] = yi_pred_num
+                else: 
+                    Xext[col] = yi_pred
+            pred_list[col] = yi_pred
+        return pred_list.iloc[:, -len(self.models):]
