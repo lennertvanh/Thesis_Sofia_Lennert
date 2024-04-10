c2d189e3d998161b42e8b94262cea9d51f003c22
lennertvanh <lennert.vanhaeren@live.be>
2024-04-10 11:32:34 +0200

    comment perm importance file

--- permutation_importance.py
+++ permutation_importance.py
@@ -96,38 +96,58 @@ class Chain(BaseEstimator):
 
 
     def importances_target(self, X, y_true, y_pred):
+
+        """Calculate the permutation importance
+
+        @param X: Input DataFrame or 2D numpy array.
+        @param y_true: true target values
+        @param y_pred: predictions for the target values
+        """
         if not isinstance(X, pd.DataFrame):
             X = pd.DataFrame(X, columns=[f"X{i}" for i in range(X.shape[1])])
         model_list = self.models.copy()
         data = pd.concat([X, y_pred], axis=1)
-        v = X.shape[1]
+        v = X.shape[1] # number of features
         feat_imp_score = []
+        # Calculate permutation feature importance for each model
         for a in range(1, y_pred.shape[1] + 1):  
             y_true_a = y_true.iloc[:, a-1]  
             y_pred_a = y_pred.iloc[:, a-1]  
             feat_imp_score_model = []
+            """
+            TO DO: make code more clean (checking type=object outside for loop for all predictors)
+            """
+            # Calculate importance of each predictor 
             for i in range(v+a-1):  
                 Xext = data.iloc[:, :a+v-1].copy()  
                 last = Xext.iloc[:, -1]
+                # Convert categorical to numerical encoding
                 if last.dtype == 'object':
                     yi_pred_num = pd.Categorical(last).codes
                     yi_pred_num = pd.Series(yi_pred_num)
                     Xext.iloc[:, -1] = yi_pred_num
+                # For the input variables from X, don't use cascading effect
+                # Shuffle feature i in Xext + predict only model a
                 if i < v: 
                     Xext.iloc[:, i] = Xext.iloc[:, i].sample(frac=1).reset_index(drop=True)
                     yi_pred_shuffled = model_list[a-1].predict(Xext)
+                # For input variables from y_pred, use the cascading effect
+                # Shuffle target i in Xext + regenerate predictions for the next targets (until model a)
                 else:
                     Xext = data.iloc[:, :i].copy()  
                     y_new = data.iloc[:, i].sample(frac=1).reset_index(drop=True)
                     Xext[y_new.name] = y_new
                     Xext_ori = Xext.copy()
                     last = Xext.iloc[:, -1]
+                    # Convert categorical to numerical encoding
                     if last.dtype == 'object':
                         yi_pred_num = pd.Categorical(last).codes
                         yi_pred_num = pd.Series(yi_pred_num)
                         Xext.iloc[:, -1] = yi_pred_num
+                    # Apply cascading effect
                     for m in range(i-v, a-1):  
                         yi_pred = model_list[m+1].predict(Xext)
+                        # Convert categorical to numerical encoding
                         if yi_pred.dtype == 'object':
                             yi_pred_num = pd.Categorical(yi_pred).codes
                             yi_pred_num = pd.Series(yi_pred_num)
@@ -139,21 +159,25 @@ class Chain(BaseEstimator):
                         Xext_ori[column_name] =  yi_pred
                     yi_pred_shuffled = Xext_ori.iloc[:, -1] 
 
+                # Remove rows with missing values in 
                 assert len(y_true_a) == len(yi_pred_shuffled)
                 missing_rows_mask = pd.Series(y_true_a).notna()  
-                y_test = y_true_a[missing_rows_mask]
+                y_true = y_true_a[missing_rows_mask]
                 y_pred_actual = y_pred_a[missing_rows_mask]
                 y_pred_shuffled_actual = yi_pred_shuffled[missing_rows_mask]
 
-                if y_test.dtype.kind in 'bifc':
-                    score1 = r2_score(y_test, y_pred_actual)
-                    score2 = r2_score(y_test, y_pred_shuffled_actual)
+                # Calculate evaluation metrics (R2 or accuracy)
+                if y_true.dtype.kind in 'bifc':
+                    score1 = r2_score(y_true, y_pred_actual)
+                    score2 = r2_score(y_true, y_pred_shuffled_actual)
                 else:
-                    score1 = accuracy_score(y_test, y_pred_actual)
-                    score2 = accuracy_score(y_test, y_pred_shuffled_actual) 
+                    score1 = accuracy_score(y_true, y_pred_actual)
+                    score2 = accuracy_score(y_true, y_pred_shuffled_actual) 
 
+                # Obtain permutation feature importance score for the input of model a
                 feat_imp_score_model.append(score1-score2)
                 
+            # Store all permutation importance lists for each model in one big list
             feat_imp_score.append(feat_imp_score_model)
 
         return feat_imp_score
